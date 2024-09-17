ed40f7f6298064421146c8e133f2d82599d34366
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-09-17 23:59:22 +0700

    fix: wrong app name in settings

--- menfess/settings.py
+++ menfess/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'whitenoise.runserve_nostatic'
+    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
